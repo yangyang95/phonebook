6d8545fd78a40bdb76dc833357cf9a0669f4a7ee
yangyang95 <yangyang.weng@gmail.com>
2017-02-23 17:34:51 +0800

    move detail information of phonebook entry to new structure

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,14 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
+#define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
+    struct detail *pDetail;
+    struct __PHONE_BOOK_ENTRY *pNext;
+} entry;
+
+typedef struct __ENTRY_DETAIL {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,8 +22,7 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
-} entry;
+} detail;
 
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
